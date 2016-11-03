6087cb67354fc48676cd4f3909f14554130da44d
unknown <wangsongyuan@bs-lp0778.corp.beisencorp.com>
2016-11-04 02:17:13 +0800

    first commit

---
+++ demo/hello.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import sys
+
+
+def get_sys_path():
+    return sys.path
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="demo",
+    version="0.10 ",
+    description=" test generate package ",
+    author=" novelwsy",
+    packages=find_packages()
+)
